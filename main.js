a6c8f51895dbb7c973427ca31f42080c72833f09
Itz_Mahi_Offical <85633027+MAHI227@users.noreply.github.com>
2022-03-07 16:09:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 x = 0;
-y - 0;
-draw_circle -"";
-draw_rect-"";
- var SpeechRecognition - window.webkitSpeechRecognition;
+y = 0;
+draw_circle = "";
+draw_rect = "";
+ var SpeechRecognition = window.webkitSpeechRecognition;
 
- var recognition - new SpeechRecognition();
+ var recognition = new SpeechRecognition();
 
  function start() 
  {
-     document.getElementById("status").innerHTML - "System is listening please speak"
+     document.getElementById("status").innerHTML = "System is listening please speak"
      recognition.start()
  }
- recognition.onresult - function(event){
+ recognition.onresult = function(event){
      console.log(event);
 
-var content - even.results[0][0].transcript;
+var content = event.results[0][0].transcript;
 
-document.getElementById("status").innerHTML - "The Speech has been recognized as:" +
+document.getElementById("status").innerHTML = "The Speech has been recognized as:" + content;
 if(content =="circle")
- }
+ 
  {
      x=Math.floor(Math.random()*900);
      y=Math.floor(Math.random()*600);
@@ -30,15 +30,16 @@ if(content =="circle")
  {
      x = Math.floor(Math.random()*900);
     y = Math.floor(Math.random()*600);
-
-    function setup(){
-        canvas = createCanva(900,600)
+ document.getElementById("status").innerHTML="Started drawing rectangle"}
+ }
+     function setup(){
+        canvas = createCanvas(900,600)
     }
 
     function draw(){
         if(draw_circle =="set")
         {
-            radius - Math.floor(Math.random()*100)
+            radius = Math.floor(Math.random()*100)
             circle(x,y,radius);
             document.getElementById("status").innerHTML ="circle is drawn."
             draw_circle="";
